149d6a8d1baa19ec7b39e782caf93675d5e5fe81
dew.maple <dew.maple@gmail.com>
2018-03-30 18:27:14 +0800

    xxx

--- samples/person/coco/coco_downloader.py
+++ samples/person/coco/coco_downloader.py
@@ -1,4 +1,5 @@
 import os
+import time
 
 import pylab
 from pycocotools.coco import COCO
@@ -27,11 +28,58 @@ def download(data_dir, subset, year, cat_nms):
         try:
             coco.download(tarDir=subset_dir, imgIds=img_ids)
             break
-        except Exception:
+        except Exception as e:
             count += 1
+            print(e)
             print("Retry {} times".format(count))
-
+        time.sleep(30)
     print("{} data downloaded, {} images. ".format(subset, len(img_ids)))
+
+    create_annotation(subset_dir, coco, cat_ids)
+
+
+def _shape_coco_2_vgg(ann, coco):
+    # VGG Image Annotator saves each image in the form:
+    # { 'filename': '28503151_5b5b7ec140_b.jpg',
+    #   'regions': {
+    #       '0': {
+    #           'region_attributes': {},
+    #           'shape_attributes': {
+    #               'all_points_x': [...],
+    #               'all_points_y': [...],
+    #               'name': 'polygon'}},
+    #       ... more regions ...
+    #   },
+    #   'size': 100202
+    # }
+    segs = ann['segmentation']
+
+    regions = {}
+    for i, seg in enumerate(segs):
+        idx = len(seg)/2
+
+        regions[i] = {
+            'region_attributes': {},
+            'shape_attributes': {
+                'all_points_x': seg[0:idx],
+                'all_points_y': seg[idx:],
+                'name': 'polygon'
+            }
+        }
+
+    img_id = ann['image_id']
+    # coco.loadImgs
+def create_annotation(tar_dir, coco, cat_ids):
+    ann_ids = coco.getAnnIds(catIds=cat_ids)
+    anns = coco.loadAnns(ann_ids)
+    annotations = []
+    count = 0
+    for ann in anns:
+
+        if count < 3:
+            print(ann)
+            print(coco.annToRLE(ann))
+        count += 1
 
 
 if __name__ == '__main__':
